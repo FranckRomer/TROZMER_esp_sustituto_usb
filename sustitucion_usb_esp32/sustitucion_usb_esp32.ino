1c89ee7b91da19c643ce6e5e52e39a3bc42119e8
Francisco Angel Romero Tepal <57637390+FranckRomer@users.noreply.github.com>
2022-05-23 17:53:19 -0500

    Se agrego dos puertos seriales

--- sustitucion_usb_esp32/sustitucion_usb_esp32.ino
+++ sustitucion_usb_esp32/sustitucion_usb_esp32.ino
@@ -9,8 +9,14 @@
 const char* ssid = "RED ACCESA";
 const char* password = "037E32E7";
 
-//const char* server = "http://192.168.1.121:3456/api/info";
-const char* server = "http://192.168.1.113:3456/esp32";
+const char* server = "http://192.168.1.121:3456/tarj";
+//const char* server = "http://192.168.1.113:3456/esp32";
+
+#define RXD1 4
+#define TXD1 2
+
+#define RXD2 16
+#define TXD2 17
 
 
 /*
@@ -20,10 +26,9 @@ const char* server = "http://192.168.1.113:3456/esp32";
 */
 void setup() {
   // Setup basico
-  Serial.begin(115200);
-  delay(50);
-  Serial2.begin(115200);
-  
+  Serial.begin(115200); // El TXD Y RXD del modulo son los de la programacion (debug)
+  Serial1.begin(115200, SERIAL_8N1, RXD1, TXD1);// serial1 pines 4 y 2
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);// serial2 pines 16 y 17 
   Serial.println(".");
   Serial.println(".");
   Serial.println(".");
@@ -40,6 +45,17 @@ void setup() {
 
 void loop() {
   //Serial.println(".");
+  if (Serial1.available()) {
+     //Serial.print(char(Serial1.read()));
+     String data = Serial1.readStringUntil('\n');
+     //String data = char(Serial1.read())
+     Serial.println(data);
+     char buffer[256];
+     StaticJsonDocument<300> doc;
+     doc["oxi"]              = data;
+     size_t n = serializeJson(doc, buffer);
+     POST_JSON(String(buffer));
+   }
   if (Serial2.available()){
     //int rawData = Serial2.read();
     //Print value in HEX
@@ -47,17 +63,13 @@ void loop() {
     //DEBUG(data);
     Serial.println(data);
     ///////////////////////////////////////////////
-//    String msm_serial2 = "";
-//    msm_serial2 = String(Serial2.read());
-//    //msm_serial2 = String((char *)msm_serial2);
-//    Serial.println(Serial2.read());
-//    
+
     char buffer[256];
     //dataSnd_string = String(json);
     StaticJsonDocument<300> doc;
     
-    doc["dispositivo"]       = "MUX";
-    doc["data"]              = data;
+    //doc["dispositivo"]       = "MUX";
+    doc["tarj"]              = data;
 
 
     size_t n = serializeJson(doc, buffer);
